7a91a6967e68f60cedaf0a036df9ed96fda39fa9
fazleh-rakib <rakibfazleh@gmail.com>
2023-12-07 15:21:06 +0600

    load country api from json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadCountries></LoadCountries>
     </div>
   );
+}
+
+function LoadCountries (){
+  const [countries,setCountries] = useState([])
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res=> res.json())
+    .then(data => setCountries(data))
+  },[])
+  return(
+    <div>
+      <h2>Load All Countries</h2>
+      <p>countries : {countries.length}</p>
+      {
+        countries.map(country => <Country name={country.name.common} capital={country.capital}></Country>)
+      }
+    </div>
+  )
+}
+function Country (props){
+  return(
+    <div className='container'>
+      <h2>Country : {props.name}</h2>
+      <h3>Capital : {props.capital}</h3>
+    </div>
+  )
 }
 
 export default App;
